cd59397b7538c936161d2999c9dd4c5e347c4f7c
victor-soeiro <victor.soeiro.araujo@gmail.com>
2024-05-20 21:49:04 -0300

    Update populate_database.py

--- tasty_delivery/scripts/populate_database.py
+++ tasty_delivery/scripts/populate_database.py
@@ -20,12 +20,12 @@ def populate():
         session.execute(
             text(
                 f'''
-                INSERT INTO users (id, username, name, email, cpf, hashed_password, is_active, is_deleted, created_at, updated_at, scopes)
+                INSERT INTO users (id, total, discount, status, is_active, is_deleted, created_at, updated_at)
                 VALUES 
-                    ('{order_1_id}', 'joao@email.com', 'João', 'joao@email.com', '11122233344', '{get_password_hash('password')}', true, false, '{datetime.utcnow()}', null, '{{admin}}'),
-                    ('{order_2_id}', 'victor@email.com', 'Victor', 'victor@email.com', '22233344455', '{get_password_hash('password')}', true, false, '{datetime.utcnow()}', null, '{{admin}}'),
-                    ('{order_3_id}', 'tais@email.com', 'Tais', 'tais@email.com', '33344455566', '{get_password_hash('password')}', true, false, '{datetime.utcnow()}', null, '{{admin}}'),
-                    ('{order_4_id}', 'augusto@email.com', 'Augusto', 'augusto@email.com', '44455566677', '{get_password_hash('password')}', true, false, '{datetime.utcnow()}', null, '{{admin}}')
+                    ('{order_1_id}', '25', '0', 'RECEBIDO', true, false, '{datetime.utcnow()}', null'),
+                    ('{order_2_id}', '50', '0', 'RECEBIDO', true, false, '{datetime.utcnow()}', null'),
+                    ('{order_3_id}', '30', '0', 'PRONTO', ' true, false, '{datetime.utcnow()}', null'),
+                    ('{order_4_id}', '40', '0', 'PRONTO', true, false, '{datetime.utcnow()}', null')
                 '''
             )
         )
