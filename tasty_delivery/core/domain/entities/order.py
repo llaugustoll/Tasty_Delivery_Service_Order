143301ee8ed575fc1bd15b12cc864643b001dfa5
victor-soeiro <victor.soeiro.araujo@gmail.com>
2024-05-20 22:56:31 -0300

    Removed Order Association

--- tasty_delivery/core/domain/entities/order.py
+++ tasty_delivery/core/domain/entities/order.py
@@ -6,19 +6,14 @@ from pydantic import BaseModel, Field
 from core.domain.entities.base import Base
 
 
-class Product(BaseModel):
-    product_id: UUID = Field()
-    quantity: int = Field(gt=0)
-
-
 class OrderIN(BaseModel):
-    products: List[Product] = Field()
+    products: List[int] = Field(default=list())
     discount: float | None = Field(0, gte=0)
     total: float = Field(gte=0)
 
 
 class OrderUpdate(BaseModel):
-    products: List[Product] | None = Field()
+    products: List[int] | None = Field()
     discount: float | None = Field(gte=0)
     total: float | None = Field(gte=0)
     status: str | None = Field(None)
@@ -27,7 +22,7 @@ class OrderUpdate(BaseModel):
 class OrderOUT(Base):
     order_id: int = Field(..., alias="order_id")
     client_id: str | None = Field()
-    products: List[Product] | None = Field()
+    products: List[int] | None = Field()
     discount: float | None = Field(gte=0)
     total: float | None = Field(gte=0)
     status: str | None = Field()
--- tasty_delivery/scripts/populate_database.py
+++ tasty_delivery/scripts/populate_database.py
@@ -1,6 +1,5 @@
 
 from datetime import datetime
-from uuid import uuid4
 
 from sqlalchemy import text
 from sqlalchemy.exc import IntegrityError
@@ -12,20 +11,15 @@ def populate():
     try:
         session = next(get_db())
 
-        order_1_id = uuid4()
-        order_2_id = uuid4()
-        order_3_id = uuid4()
-        order_4_id = uuid4()
-
         session.execute(
             text(
                 f'''
                 INSERT INTO orders (id, total, discount, status, is_active, is_deleted, created_at, updated_at)
                 VALUES 
-                    (1, '25', '0', 'RECEBIDO', true, false, '{datetime.utcnow()}', null),
-                    (2, '50', '0', 'RECEBIDO', true, false, '{datetime.utcnow()}', null),
-                    (3, '30', '0', 'PRONTO', true, false, '{datetime.utcnow()}', null),
-                    (4, '40', '0', 'PRONTO', true, false, '{datetime.utcnow()}', null)
+                    (1, '25', '0', 'RECEBIDO', true, false, '{datetime.utcnow()}', null'),
+                    (2, '50', '0', 'RECEBIDO', true, false, '{datetime.utcnow()}', null'),
+                    (3, '30', '0', 'PRONTO', ' true, false, '{datetime.utcnow()}', null'),
+                    (4, '40', '0', 'PRONTO', true, false, '{datetime.utcnow()}', null')
                 '''
             )
         )
